d6abc7891d541fe458de6bfabbbd4728850febca
玉村柊希 <syuuki0105@gmail.com>
2018-09-18 13:34:55 +0900

    シューティング ゲーム Swift

---
+++ GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  ShootingGame
+//
+//  Created by 玉村柊希 on 2018/09/18.
+//  Copyright © 2018年 玉村柊希. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene
+{
+    
+    override func didMove(to view: SKView)
+    {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+        // Called before each frame is rendered
+    }
+}
